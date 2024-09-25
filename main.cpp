e8804ca5ca32a010df36673edaf101b4c60b3372
nulpTaras <taras.neridnyi.ri.2024@lpnu.ua>
2024-09-25 20:10:19 +0300

    changed names of vars

--- main.cpp
+++ main.cpp
@@ -6,23 +6,23 @@ using namespace std;
 int main()
 {
 
-    double x , left , result , condition_result;
+    double x , A , result , B;
     cout << "input number : "; cin >> x;
 
-    left = pow(x,2) + sin((4* x) / 3); // розвязок лівої частини
+    A = pow(x,2) + sin((4* x) / 3); // розвязок лівої частини
 
     // скорочена
-    if (x < -5)  { condition_result = 1/(tan(x)); }
-    if (x >= -5 && x <= 0)  { condition_result = 4 - (pow(x, 2) / 2); }
-    if (x > 0)  { condition_result = log(pow(x, 2)) - ( (4*x)/3 ); }
+    if (x < -5)  { B = 1/(tan(x)); }
+    if (x >= -5 && x <= 0)  { B = 4 - (pow(x, 2) / 2); }
+    if (x > 0)  { B = log(pow(x, 2)) - ( (4*x)/3 ); }
 
-    result = left + condition_result;
+    result = A + B;
     cout << "1) y = " << result << endl;
 
     // повна
-    if( x < -5 )  { condition_result = 1/(tan(x)); }
-    else if (x > 0)  { condition_result = log(pow(x, 2)) - ( (4*x)/3 ); }
-    else  { condition_result = 4 - (pow(x, 2) / 2 ); }
+    if( x < -5 )  { B = 1/(tan(x)); }
+    else if (x > 0)  { B = log(pow(x, 2)) - ( (4*x)/3 ); }
+    else  { B = 4 - (pow(x, 2) / 2 ); }
 
     cout << "2) y = " << result << endl;
 
